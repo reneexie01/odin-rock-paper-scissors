8b61ccb85eb68575deb35b11cfdc1f04478af792
Renee <156127959+reneexie01@users.noreply.github.com>
2024-03-19 06:20:40 +1100

    Added rock, paper, scissors player choice buttons

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ const startGameBtn = document.querySelector("#start-game");
 startGameBtn.addEventListener("click", startGame);
 const resetGameBtn = document.querySelector("#reset-game");
 resetGameBtn.addEventListener("click", resetGame);
+
+const rockBtn = document.querySelector("#rock-btn");
+rockBtn.addEventListener("click", () => startGameChoiceBtns("ROCK"));
+const paperBtn = document.querySelector("#paper-btn");
+paperBtn.addEventListener("click", () => startGameChoiceBtns("PAPER"));
+const scissorsBtn = document.querySelector("#scissors-btn");
+scissorsBtn.addEventListener("click", () => startGameChoiceBtns("SCISSORS"));
+
 let playerSelection;
 let computerSelection;
 let playerWins = 0;
@@ -12,6 +20,86 @@ const printPlayerChoice = document.querySelector("#player-choice");
 const printComputerChoice = document.querySelector("#computer-choice");
 const printGameResult = document.querySelector("#game-result");
 const printScore = document.querySelector("#score");
+
+function startGameChoiceBtns(playerSelection) {
+    const computerSelection = computerChoice();
+    let gameOutcome;
+    
+    printPlayerChoice.innerHTML = `<strong>Player choice:</strong> ${playerSelection}`;
+    printComputerChoice.innerHTML = `<strong>Computer choice:</strong> ${computerSelection}`;
+    printGameResult.innerHTML = `<strong>Game result:</strong> ${playGame(playerSelection, computerSelection)}`;
+    
+    function playGame(playerSelection, computerSelection) {
+        if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
+            gameOutcome = "loss";
+            return "You lose! ROCK beats SCISSORS.";
+        } else if (computerSelection === "ROCK" && playerSelection === "ROCK") {
+            gameOutcome = "tie";
+            return "Tie! Pick again.";
+        } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
+            gameOutcome = "win";
+            return "You win! PAPER beats ROCK.";
+        } else if (computerSelection === "SCISSORS" && playerSelection === "SCISSORS") {
+            gameOutcome = "tie";
+            return "Tie! Pick again.";
+        } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
+            gameOutcome = "win";
+            return "You win! ROCK beats SCISSORS.";
+        } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
+            gameOutcome = "loss";
+            return "You lose! SCISSORS beats PAPER.";
+        } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
+            gameOutcome = "win";
+            return "You win! SCISSORS beats PAPER.";
+        } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
+            gameOutcome = "loss";
+            return "You lose! PAPER beats ROCK.";
+        } else if (computerSelection === "PAPER" && playerSelection === "PAPER") {
+            gameOutcome = "tie";
+            return "Tie! Pick again.";
+        } else {
+            gameOutcome = "invalid";
+            return "Invalid option selected. Try again.";
+        }
+    }
+    
+    scoreCount(gameOutcome);
+    
+    console.log("Player: ", playerSelection);
+    console.log("Computer: ", computerSelection);
+    console.log(playGame(playerSelection, computerSelection))
+    console.log("Player wins: ", playerWins);
+    console.log("Computer wins: ", computerWins);
+    console.log("Game number: ", gameNumber);
+    printScore.innerHTML = `
+    <p><strong>Score: </strong><p> 
+    <p>PLAYER wins: ${playerWins} | COMPUTER wins: ${computerWins}</p>
+    <p><strong>Games Played:</strong> ${gameNumber}</p>
+    `
+    
+    function scoreCount(gameOutcome) {
+        if (gameOutcome === "loss") {
+            gameNumber++;
+            computerWins++;
+        } else if (gameOutcome === "win") {
+            gameNumber++;
+            playerWins++;
+        } else {
+            gameNumber++;
+        }
+    }
+    
+    function computerChoice() {
+        const randomNumber = Math.floor((Math.random() * 3) + 1);
+        if (randomNumber === 1) {
+            return "ROCK";
+        } else if (randomNumber === 2) {
+            return "SCISSORS";
+        } else {
+            return "PAPER";
+        }
+    }
+      }
 
 function startGame() {
 const inputValue = document.querySelector("#player-input").value;
